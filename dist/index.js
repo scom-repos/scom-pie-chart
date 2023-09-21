27a8eee06dd14a6b88ce02d2272c24298be4b47e
celina-ijs <celina.dev@instajs.com>
2023-09-21 14:48:11 +0700

    Update to use FormatUtils

--- dist/index.js
+++ dist/index.js
@@ -33,54 +33,54 @@ define("@scom/scom-pie-chart/global/interfaces.ts", ["require", "exports"], func
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
 });
-define("@scom/scom-pie-chart/global/utils.ts", ["require", "exports", "@ijstech/eth-wallet"], function (require, exports, eth_wallet_1) {
+define("@scom/scom-pie-chart/global/utils.ts", ["require", "exports", "@ijstech/components"], function (require, exports, components_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.formatNumberWithSeparators = exports.formatNumberByFormat = exports.formatNumber = void 0;
+    exports.formatNumberByFormat = exports.formatNumber = void 0;
     const formatNumber = (num, options) => {
         if (num === null)
             return '-';
         const { decimals, format, percentValues } = options || {};
         if (percentValues) {
-            return `${(0, exports.formatNumberWithSeparators)(num, { precision: 2 })}%`;
+            return `${components_1.FormatUtils.formatNumber(num, { decimalFigures: 2 })}%`;
         }
         if (format) {
             return (0, exports.formatNumberByFormat)(num, format);
         }
         const absNum = Math.abs(num);
         if (absNum >= 1000000000) {
-            return (0, exports.formatNumberWithSeparators)((num / 1000000000), { precision: decimals || 3 }) + 'B';
+            return components_1.FormatUtils.formatNumber((num / 1000000000), { decimalFigures: decimals || 3 }) + 'B';
         }
         if (absNum >= 1000000) {
-            return (0, exports.formatNumberWithSeparators)((num / 1000000), { precision: decimals || 3 }) + 'M';
+            return components_1.FormatUtils.formatNumber((num / 1000000), { decimalFigures: decimals || 3 }) + 'M';
         }
         if (absNum >= 1000) {
-            return (0, exports.formatNumberWithSeparators)((num / 1000), { precision: decimals || 3 }) + 'K';
+            return components_1.FormatUtils.formatNumber((num / 1000), { decimalFigures: decimals || 3 }) + 'K';
         }
         if (absNum < 0.0000001) {
-            return (0, exports.formatNumberWithSeparators)(num, { precision: 0 });
+            return components_1.FormatUtils.formatNumber(num, { decimalFigures: 0 });
         }
         if (absNum < 0.00001) {
-            return (0, exports.formatNumberWithSeparators)(num, { precision: 6 });
+            return components_1.FormatUtils.formatNumber(num, { decimalFigures: 6 });
         }
         if (absNum < 0.001) {
-            return (0, exports.formatNumberWithSeparators)(num, { precision: 4 });
+            return components_1.FormatUtils.formatNumber(num, { decimalFigures: 4 });
         }
         if (absNum < 1) {
-            return (0, exports.formatNumberWithSeparators)(num, { precision: 4 });
+            return components_1.FormatUtils.formatNumber(num, { decimalFigures: 4 });
         }
-        return (0, exports.formatNumberWithSeparators)(num, { precision: 2 });
+        return components_1.FormatUtils.formatNumber(num, { decimalFigures: 2 });
     };
     exports.formatNumber = formatNumber;
     const formatNumberByFormat = (num, format, separators) => {
         if (!format)
-            return (0, exports.formatNumberWithSeparators)(num, { precision: 0 });
-        const decimalPlaces = format.split('.')[1] ? format.split('.')[1].length : 0;
+            return components_1.FormatUtils.formatNumber(num, { decimalFigures: 0 });
+        const decimalFigures = format.split('.')[1] ? format.split('.')[1].length : 0;
         if (format.includes('%')) {
-            return (0, exports.formatNumberWithSeparators)((num * 100), { precision: decimalPlaces }) + '%';
+            return components_1.FormatUtils.formatNumber((num * 100), { decimalFigures }) + '%';
         }
         const currencySymbol = format.indexOf('$') !== -1 ? '$' : '';
-        const roundedNum = (0, exports.formatNumberWithSeparators)(num, { precision: decimalPlaces });
+        const roundedNum = components_1.FormatUtils.formatNumber(num, { decimalFigures });
         if (separators || !(format.includes('m') || format.includes('a'))) {
             return format.indexOf('$') === 0 ? `${currencySymbol}${roundedNum}` : `${roundedNum}${currencySymbol}`;
         }
@@ -90,34 +90,33 @@ define("@scom/scom-pie-chart/global/utils.ts", ["require", "exports", "@ijstech/
         return `${currencySymbol}${integerPart}`;
     };
     exports.formatNumberByFormat = formatNumberByFormat;
-    const formatNumberWithSeparators = (value, options) => {
-        let bigValue;
-        if (value instanceof eth_wallet_1.BigNumber) {
-            bigValue = value;
-        }
-        else {
-            bigValue = new eth_wallet_1.BigNumber(value);
-        }
-        if (bigValue.isNaN() || !bigValue.isFinite()) {
-            return '0';
-        }
-        if (options.precision || options.precision === 0) {
-            let outputStr = '';
-            if (bigValue.gte(1)) {
-                outputStr = bigValue.toFormat(options.precision, options.roundingMode || eth_wallet_1.BigNumber.ROUND_HALF_CEIL);
-            }
-            else {
-                outputStr = bigValue.toNumber().toLocaleString('en-US', { maximumSignificantDigits: options.precision || 2 });
-            }
-            if (outputStr.length > 18) {
-                outputStr = outputStr.substring(0, 18) + '...';
-            }
-            return outputStr;
-        }
-        return bigValue.toFormat();
-    };
-    exports.formatNumberWithSeparators = formatNumberWithSeparators;
 });
+// export const FormatUtils.formatNumber = (value: number | string | BigNumber, options: IFormatNumberOptions): string => {
+//   let bigValue: BigNumber;
+//   if (value instanceof BigNumber) {
+//     bigValue = value;
+//   }
+//   else {
+//     bigValue = new BigNumber(value);
+//   }
+//   if (bigValue.isNaN() || !bigValue.isFinite()) {
+//     return '0';
+//   }
+//   if (options.precision || options.precision === 0) {
+//     let outputStr = '';
+//     if (bigValue.gte(1)) {
+//       outputStr = bigValue.toFormat(options.precision, options.roundingMode || BigNumber.ROUND_HALF_CEIL);
+//     }
+//     else {
+//       outputStr = bigValue.toNumber().toLocaleString('en-US', { maximumSignificantDigits: options.precision || 2 });
+//     }
+//     if (outputStr.length > 18) {
+//       outputStr = outputStr.substring(0, 18) + '...';
+//     }
+//     return outputStr;
+//   }
+//   return bigValue.toFormat();
+// }
 define("@scom/scom-pie-chart/global/index.ts", ["require", "exports", "@scom/scom-pie-chart/global/interfaces.ts", "@scom/scom-pie-chart/global/utils.ts"], function (require, exports, interfaces_1, utils_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -125,26 +124,26 @@ define("@scom/scom-pie-chart/global/index.ts", ["require", "exports", "@scom/sco
     __exportStar(interfaces_1, exports);
     __exportStar(utils_1, exports);
 });
-define("@scom/scom-pie-chart/index.css.ts", ["require", "exports", "@ijstech/components"], function (require, exports, components_1) {
+define("@scom/scom-pie-chart/index.css.ts", ["require", "exports", "@ijstech/components"], function (require, exports, components_2) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.chartStyle = exports.containerStyle = void 0;
-    const Theme = components_1.Styles.Theme.ThemeVars;
-    exports.containerStyle = components_1.Styles.style({
+    const Theme = components_2.Styles.Theme.ThemeVars;
+    exports.containerStyle = components_2.Styles.style({
         width: 'var(--layout-container-width)',
         maxWidth: 'var(--layout-container-max_width)',
         textAlign: 'var(--layout-container-text_align)',
         margin: '0 auto',
         padding: 10
     });
-    exports.chartStyle = components_1.Styles.style({
+    exports.chartStyle = components_2.Styles.style({
         display: 'block',
     });
 });
-define("@scom/scom-pie-chart/assets.ts", ["require", "exports", "@ijstech/components"], function (require, exports, components_2) {
+define("@scom/scom-pie-chart/assets.ts", ["require", "exports", "@ijstech/components"], function (require, exports, components_3) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    let moduleDir = components_2.application.currentModuleDir;
+    let moduleDir = components_3.application.currentModuleDir;
     function fullPath(path) {
         if (path.indexOf('://') > 0)
             return path;
@@ -429,10 +428,10 @@ define("@scom/scom-pie-chart/formSchema.ts", ["require", "exports"], function (r
     }
     exports.getEmbedderSchema = getEmbedderSchema;
 });
-define("@scom/scom-pie-chart/dataOptionsForm.tsx", ["require", "exports", "@ijstech/components"], function (require, exports, components_3) {
+define("@scom/scom-pie-chart/dataOptionsForm.tsx", ["require", "exports", "@ijstech/components"], function (require, exports, components_4) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    let ScomPieChartDataOptionsForm = class ScomPieChartDataOptionsForm extends components_3.Module {
+    let ScomPieChartDataOptionsForm = class ScomPieChartDataOptionsForm extends components_4.Module {
         constructor(parent, options) {
             super(parent, options);
         }
@@ -493,16 +492,16 @@ define("@scom/scom-pie-chart/dataOptionsForm.tsx", ["require", "exports", "@ijst
         }
     };
     ScomPieChartDataOptionsForm = __decorate([
-        components_3.customModule,
-        (0, components_3.customElements)('i-scom-pie-chart-data-options-form')
+        components_4.customModule,
+        (0, components_4.customElements)('i-scom-pie-chart-data-options-form')
     ], ScomPieChartDataOptionsForm);
     exports.default = ScomPieChartDataOptionsForm;
 });
-define("@scom/scom-pie-chart", ["require", "exports", "@ijstech/components", "@scom/scom-pie-chart/global/index.ts", "@scom/scom-pie-chart/index.css.ts", "@scom/scom-pie-chart/assets.ts", "@scom/scom-pie-chart/data.json.ts", "@scom/scom-chart-data-source-setup", "@scom/scom-pie-chart/formSchema.ts", "@scom/scom-pie-chart/dataOptionsForm.tsx"], function (require, exports, components_4, index_1, index_css_1, assets_1, data_json_1, scom_chart_data_source_setup_1, formSchema_1, dataOptionsForm_1) {
+define("@scom/scom-pie-chart", ["require", "exports", "@ijstech/components", "@scom/scom-pie-chart/global/index.ts", "@scom/scom-pie-chart/index.css.ts", "@scom/scom-pie-chart/assets.ts", "@scom/scom-pie-chart/data.json.ts", "@scom/scom-chart-data-source-setup", "@scom/scom-pie-chart/formSchema.ts", "@scom/scom-pie-chart/dataOptionsForm.tsx"], function (require, exports, components_5, index_1, index_css_1, assets_1, data_json_1, scom_chart_data_source_setup_1, formSchema_1, dataOptionsForm_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    const Theme = components_4.Styles.Theme.ThemeVars;
-    const currentTheme = components_4.Styles.Theme.currentTheme;
+    const Theme = components_5.Styles.Theme.ThemeVars;
+    const currentTheme = components_5.Styles.Theme.currentTheme;
     const DefaultData = {
         dataSource: scom_chart_data_source_setup_1.DataSource.Dune,
         queryId: '',
@@ -511,7 +510,7 @@ define("@scom/scom-pie-chart", ["require", "exports", "@ijstech/components", "@s
         options: undefined,
         mode: scom_chart_data_source_setup_1.ModeType.LIVE
     };
-    let ScomPieChart = class ScomPieChart extends components_4.Module {
+    let ScomPieChart = class ScomPieChart extends components_5.Module {
         static async create(options, parent) {
             let self = new this(parent, options);
             await self.ready();
@@ -630,17 +629,17 @@ define("@scom/scom-pie-chart", ["require", "exports", "@ijstech/components", "@s
                     },
                     customUI: {
                         render: (data, onConfirm, onChange) => {
-                            const vstack = new components_4.VStack(null, { gap: '1rem' });
+                            const vstack = new components_5.VStack(null, { gap: '1rem' });
                             const dataSourceSetup = new scom_chart_data_source_setup_1.default(null, Object.assign(Object.assign({}, this._data), { chartData: JSON.stringify(this.pieChartData), onCustomDataChanged: async (dataSourceSetupData) => {
                                     if (onChange) {
                                         onChange(true, Object.assign(Object.assign({}, this._data), dataSourceSetupData));
                                     }
                                 } }));
-                            const hstackBtnConfirm = new components_4.HStack(null, {
+                            const hstackBtnConfirm = new components_5.HStack(null, {
                                 verticalAlignment: 'center',
                                 horizontalAlignment: 'end'
                             });
-                            const button = new components_4.Button(null, {
+                            const button = new components_5.Button(null, {
                                 caption: 'Confirm',
                                 width: 'auto',
                                 height: 40,
@@ -902,7 +901,7 @@ define("@scom/scom-pie-chart", ["require", "exports", "@ijstech/components", "@s
                 ]
             };
             this.pnlPieChart.clearInnerHTML();
-            const pieChart = new components_4.PieChart(this.pnlPieChart, {
+            const pieChart = new components_5.PieChart(this.pnlPieChart, {
                 data: _chartData,
                 width: '100%',
                 height: '100%'
@@ -959,8 +958,8 @@ define("@scom/scom-pie-chart", ["require", "exports", "@ijstech/components", "@s
         }
     };
     ScomPieChart = __decorate([
-        components_4.customModule,
-        (0, components_4.customElements)('i-scom-pie-chart')
+        components_5.customModule,
+        (0, components_5.customElements)('i-scom-pie-chart')
     ], ScomPieChart);
     exports.default = ScomPieChart;
 });
